f87dcc4a1eedf572ca328c502dfd53bf77dfccc7
anawchakk99 <anafranwicha@gmail.com>
2026-05-31 22:10:05 -0400

    actualizo servicios clima

--- probar_clima.py
+++ probar_clima.py
@@ -1,4 +1,20 @@
-from backend.app.services.clima import sugerir_outfit
+import requests
+import os
+from dotenv import load_dotenv
 
-resultado = sugerir_outfit("Santiago", "casual")
-print(resultado)
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+response = requests.get(
+    BASE_URL,
+    params={
+        "q": "Santiago",
+        "appid": API_KEY,
+        "units": "metric",
+        "lang": "es"
+    }
+)
+
+print(response.json())
